1bb49b118608ab4b4dc3f27b267f53ebfffa5003
Nikita <nikitatorok002@gmail.com>
2022-03-03 15:07:58 +0700

    Edit metod

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -12,10 +12,10 @@ namespace lab2
                 RomanNumber number_2 = new RomanNumber(9);
                 RomanNumber number_3 = new RomanNumber(26);
                 RomanNumber[] numbers = { number_1, number_3, number_3 };
-                Console.WriteLine($"Сложение: {number_1} + {number_3} = {RomanNumber.Add(number_1, number_3).ToString()}");
-                Console.WriteLine($"Деление: {number_1} / {number_2} = {RomanNumber.Div(number_1, number_2).ToString()}");
-                Console.WriteLine($"Вычитание: {number_1} - {number_3} = {RomanNumber.Sub(number_1, number_3).ToString()}");
-                Console.WriteLine($"Умножение: {number_2} * {number_3} = {RomanNumber.Mul(number_2, number_3).ToString()}");
+                Console.WriteLine($"Сложение: {number_1} + {number_3} = {(number_1 + number_3).ToString()}");
+                Console.WriteLine($"Деление: {number_1} / {number_2} = {(number_1 / number_2).ToString()}");
+                Console.WriteLine($"Вычитание: {number_1} - {number_3} = {(number_1 - number_3).ToString()}");
+                Console.WriteLine($"Умножение: {number_2} * {number_3} = {(number_2 * number_3).ToString()}");
    
                 Console.WriteLine("Сортировка массива: Исходные данные");
                 foreach (var i in numbers)
